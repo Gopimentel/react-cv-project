253198fc60b696f31ed9a718e11c1d7bc11dafe9
Gopimentel <golpimentel@gmail.com>
2019-03-24 18:49:05 +0000

    Initial Commit BoilerPlate project with Dev and Prod environments ready

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const text = 'Boilerplate Project is working';
+const Test = () => <p>{text}</p>;
+ReactDOM.render(<Test />, document.getElementById('app'));
