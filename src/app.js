fe72598fae865da84456e9c27bedee1333b20140
Gopimentel <golpimentel@gmail.com>
2019-03-24 19:37:31 +0000

    fix css was not being loaded

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
 
 const text = 'Boilerplate Project is working';
 const Test = () => <p>{text}</p>;
