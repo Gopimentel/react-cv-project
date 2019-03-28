df2e51eab58a9fc7e3017cb09a18078d8b7c4706
Gopimentel <golpimentel@gmail.com>
2019-03-28 14:09:29 +0000

    media query added for ie

--- src/components/CVPage.js
+++ src/components/CVPage.js
@@ -9,7 +9,7 @@ import SkillSection from "./SkillSection";
 
 
 const technologies = "This page has been developed using React as Javascript Framework, SASS - for stylying, Webpack for bundling (of Javascript and CSS files) and Babel for code transpiling ( to transform modern Javascript into ES5 to be run in older browsers)."
-const tested = "Page tested in : Chrome, IE11, Mozila Firefox X.1 , Iphone 5s ."
+const tested = "Page tested in : Windows with Chrome 73.0., IE11, Mozila Firefox 65.0 and in Iphone 5s with iOS 12.1.2  with Chrome and Safari."
 
 const CVPage = () => (
 <div className="main-div">
